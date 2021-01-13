a600382267888721ca832c0e6e04c78d22d38051
Jakub Kaláb <jakubkalab@gmail.com>
2021-01-13 12:06:45 +0100

    added deploy script

---
+++ scripts/deploy.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+set -euf -o pipefail
+
+if [ $1 == "vaccination" ]; then
+  export REACT_APP_TYPE='vaccination'
+  export REACT_APP_BACKEND_URL='https://vaccination-form-backend.production-e.asrv.cz'
+
+  deploy_dir='vaccination-form-frontend'
+else
+  export REACT_APP_TYPE='testing'
+  export REACT_APP_BACKEND_URL='https://covid-form-backend.production-e.asrv.cz'
+
+  deploy_dir='covid-form-frontend'
+fi
+
+servers=(production1-tth production2-tth)
+deploy_to="deploy/spital/$deploy_dir"
+timestamp="$(date +%Y%m%d%H%M)"
+keep_releases=5
+
+echo "app type: $REACT_APP_TYPE"
+echo "backend url: $REACT_APP_BACKEND_URL"
+echo "deploying to $deploy_to as release $timestamp"
+
+yarn build
+
+for server in ${servers[@]}; do
+  echo "deploying to $server"
+  scp -r build $server:$deploy_to/releases/$timestamp > /dev/null
+  ssh -T $server <<EOF
+    cd $deploy_to
+    rm current
+    ln -s releases/$timestamp current
+    cd releases
+    ls -t | tail -n +$(($keep_releases + 1)) | xargs rm -r
+EOF
+done
